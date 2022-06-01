d15756fe9b1656551f3365fb6ee9a4584f5e2cb1
Gustavo Fontenele <gustavoafo171@gmail.com>
2022-06-01 12:30:26 -0300

    enunciado_ex09

---
+++ ex09.py
@@ -0,0 +1,5 @@
+"""
+Faça um programa que mostre ao usuário um menu com 4 opções de operações matemáticas
+(as básicas). O usuário escolhe uma das opções e o seu programa então pede dois valores numéricos
+e realiza a operaçõa, mostrando o resultado e saindo.
+"""
